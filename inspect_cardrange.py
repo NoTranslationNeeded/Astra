2e47301c3b225eed3f02985ccce352b246b5c709
MED1AN0 <99SANGSANG99@GMAIL.COM>
2025-11-28 13:45:02 +0900

    Chore: Clean up deleted files and update project state

--- inspect_cardrange.py
+++
@@ -1,5 +0,0 @@
-import ompeval
-from ompeval import CardRange
-
-cr = CardRange("2h")
-print(dir(cr))
--- inspect_handevaluator.py
+++
@@ -1,9 +0,0 @@
-import ompeval
-from ompeval import HandEvaluator
-
-print(dir(HandEvaluator))
-evaluator = HandEvaluator()
-try:
-    evaluator.evaluate(12345)
-except Exception as e:
-    print("evaluate(int) error:", e)
